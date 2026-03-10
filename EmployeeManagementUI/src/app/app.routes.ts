e9277ee4b2ba30b7e14862a81ccc4800ea04fc5a
Mukul Choudhari <mchoudhari2004@gmail.com>
2026-03-10 12:59:19 +0530

    work

--- EmployeeManagementUI/src/app/app.routes.ts
+++ EmployeeManagementUI/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { EmployeeForm } from './components/employee-form/employee-form';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  {
+    path: 'employees',
+    component: EmployeeList,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'employees/add',
+    component: EmployeeForm,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'employees/edit/:id',
+    component: EmployeeForm,
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '/login' }
+];
