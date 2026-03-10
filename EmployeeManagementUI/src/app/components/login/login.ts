c34d2eb3f7038a6128953e7f8ad2ef023d1c9cc0
Mukul Choudhari <mchoudhari2004@gmail.com>
2026-03-10 13:02:57 +0530

    work

--- EmployeeManagementUI/src/app/components/login/login.ts
+++ EmployeeManagementUI/src/app/components/login/login.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { LoginDto } from '../../models/employee.model';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class Login {}
+export class Login {
+  loginData: LoginDto = {
+    email: '',
+    password: ''
+  };
+
+  errorMessage: string = '';
+  loading: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin(): void {
+    this.loading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.router.navigate(['/employees']);
+        } else {
+          this.errorMessage = response.message;
+        }
+        this.loading = false;
+      },
+      error: (err) => {
+        this.errorMessage = 'Invalid email or password!';
+        this.loading = false;
+      }
+    });
+  }
+}
